17461796a6e21ad1df2ce53e9c48a546fdf373ba
Kadin Pegram <kepegram1@gmail.com>
2024-09-10 11:35:57 -0500

    KP | 2nd | Added authentication screens/initialized convex db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello World
-    </div>
+    <AuthScreen />
   );
 }
